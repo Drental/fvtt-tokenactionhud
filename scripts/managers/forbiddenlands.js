013d7da837aef48d9eee5358be15613e9d7d57ba
Drental <gunnar.busch@googlemail.com>
2021-08-01 19:18:22 +0200

    action handler for Forbidden Lands Co-authored-by: Lyle Hayhurst <lhayhurst@users.noreply.github.com>

---
+++ scripts/managers/forbiddenlands.js
@@ -0,0 +1,34 @@
+import { SystemManager } from './manager.js';
+import { ActionHandlerForbiddenlands as ActionHandler } from '../actions/forbiddenlands/forbiddenlands-actions.js'
+import { RollHandlerBaseForbiddenlands as Core } from '../rollHandlers/forbiddenLands/forbiddenlands-base.js';
+import * as settings from '../settings/forbiddenlands-settings.js'
+
+export class ForbiddenLandsSystemManager extends SystemManager {
+
+    constructor(appName) {
+        super(appName);
+    }
+
+    /** @override */
+    doGetActionHandler(filterManager, categoryManager) {
+        let actionHandler = new ActionHandler(filterManager, categoryManager);
+        return actionHandler;
+    }
+
+    /** @override */
+    getAvailableRollHandlers() {
+        let choices = { 'core': 'Core Forbidden Lands' };
+
+        return choices;
+    }
+
+    /** @override */
+    doGetRollHandler(handlerId) {
+        return new Core();
+    }
+
+    /** @override */
+    doRegisterSettings(appName, updateFunc) {
+        settings.register(appName, updateFunc);
+    }
+}
