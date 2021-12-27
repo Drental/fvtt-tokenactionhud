e77fb0028b507368e8a66388dd9ff48bf505bd33
supervj <64861570+supervj@users.noreply.github.com>
2021-12-27 15:40:40 -0500

    Update SW5e to bring in line with DND5e
    Updates to add compatability based on DND 1.5.6 core

---
+++ scripts/actions/sw5e/sw5e-actions-by-type.js
@@ -0,0 +1,111 @@
+import { ActionHandlerSW5e } from "./sw5e-actions.js";
+import * as settings from "../../settings.js";
+
+export class ActionHandlerSW5eGroupByType extends ActionHandlerSW5e {
+  /** @override */
+  async _buildCategories(token) {
+    const api = game.modules.get("character-actions-list-5e").api;
+    const actionData = await api.getActorActionsData(token.actor);
+
+    return [
+      this._buildActionCategory(token, "actions", actionData.action),
+      this._buildActionCategory(token, "bonusActions", actionData.bonus),
+      this._buildActionCategory(token, "reactions", actionData.reaction),
+      this._buildActionCategory(token, "crewActions", actionData.crew),
+      this._buildActionCategory(
+        token,
+        "legendaryActions",
+        actionData.legendary
+      ),
+      this._buildActionCategory(token, "lairActions", actionData.lair),
+      this._buildActionCategory(token, "specialActions", actionData.other),
+      this._buildSkillsCategory(token),
+      this._buildAbilitiesCategory(token),
+      this._buildEffectsCategory(token),
+      this._buildConditionsCategory(token),
+      this._buildUtilityCategory(token),
+    ];
+  }
+
+  _buildActionCategory(token, title, actions) {
+    const category = this.initializeEmptyCategory(title);
+    category.name = this.i18n(`tokenactionhud.${title}`);
+
+    let powers = [];
+    for (const a of actions) {
+      if (a.type == "power") {
+        powers.push(a);
+        actions.delete(a);
+      }
+    }
+    if (
+      token.actor.data.type === "character" ||
+      !settings.get("showAllNpcItems")
+    ) {
+      powers = this._filterNonpreparedPowers(powers);
+    }
+    powers = this._sortPowersByLevel(powers);
+    powers = this._categorisePowers(token.actor, token.id, powers);
+
+    this._subCategorizeEquipment(
+      token,
+      category,
+      actions,
+      this.i18n("tokenactionhud.weapons"),
+      (i) => i.type == "weapon"
+    );
+    this._subCategorizeEquipment(
+      token,
+      category,
+      actions,
+      this.i18n("tokenactionhud.equipment"),
+      (i) => i.type == "equipment"
+    );
+    this._subCategorizeEquipment(
+      token,
+      category,
+      actions,
+      this.i18n("tokenactionhud.consumables"),
+      (i) => i.type == "consumable"
+    );
+    this._subCategorizeEquipment(
+      token,
+      category,
+      actions,
+      this.i18n("tokenactionhud.tools"),
+      (i) => i.type == "tool"
+    );
+    this._subCategorizeEquipment(
+      token,
+      category,
+      actions,
+      this.i18n("tokenactionhud.other"),
+      (i) => true
+    );
+
+    for (const subCat of powers.subcategories) {
+      this._combineSubcategoryWithCategory(category, subCat.name, subCat);
+    }
+
+    return category;
+  }
+
+  // Adds actions matching predicate to a new subcategory, and returns it.
+  // Deletes matches from the actions set as it does so.
+  _subCategorizeEquipment(token, category, actions, title, predicate) {
+    const subcat = this.initializeEmptySubcategory();
+    const matches = new Set();
+    for (const a of actions) {
+      if (predicate(a)) {
+        matches.add(a);
+        actions.delete(a);
+      }
+    }
+    if (matches.size > 0) {
+      subcat.actions = [...matches].map((i) =>
+        this._buildEquipmentItem(token.id, token.actor, "item", i)
+      );
+      this._combineSubcategoryWithCategory(category, title, subcat);
+    }
+  }
+}
--- scripts/managers/sw5e.js
+++ scripts/managers/sw5e.js
@@ -1,33 +1,78 @@
 import { SystemManager } from "./manager.js";
-import { ActionHandlerSw5e as ActionHandler } from "../actions/sw5e/sw5e-actions.js";
-import { RollHandlerBaseSw5e as Core } from "../rollHandlers/sw5e/sw5e-base.js";
-import * as settings from "../settings/sw5e-settings.js";
+import { ActionHandlerSW5e as ActionHandler } from "../actions/sw5e/sw5e-actions.js";
+import { ActionHandlerSW5eGroupByType } from "../actions/sw5e/sw5e-actions-by-type.js";
+import { MagicItemsPreRollHandler } from "../rollHandlers/sw5e/pre-magicItems.js";
+import { MagicItemActionListExtender } from "../actions/magicItemsExtender.js";
+import { RollHandlerBaseSW5e as Core } from "../rollHandlers/sw5e/sw5e-base.js";
+import { RollHandlerMidiQolSW5e as MidiQolSW5e } from "../rollHandlers/sw5e/sw5e-midiqol.js";
+import { RollHandlerObsidianSW5e as ObsidianSW5e } from "../rollHandlers/sw5e/sw5e-obsidian.js";
+import * as settings from "../settings.js";
+import * as systemSettings from "../settings/sw5e-settings.js";
 
-export class Sw5eSystemManager extends SystemManager {
+export class SW5eSystemManager extends SystemManager {
   constructor(appName) {
     super(appName);
   }
 
   /** @override */
   doGetActionHandler(filterManager, categoryManager) {
-    let actionHandler = new ActionHandler(filterManager, categoryManager);
+    let actionHandler;
+    if (
+      SystemManager.isModuleActive("character-actions-list-5e") &&
+      settings.get("useActionList")
+    ) {
+      actionHandler = new ActionHandlerSW5eGroupByType(
+        filterManager,
+        categoryManager
+      );
+    } else {
+      actionHandler = new ActionHandler(filterManager, categoryManager);
+    }
+
+    if (SystemManager.isModuleActive("magicitems"))
+      actionHandler.addFurtherActionHandler(new MagicItemActionListExtender());
+
     return actionHandler;
   }
 
   /** @override */
   getAvailableRollHandlers() {
-    let choices = { core: "Core Star Wars 5e" };
+    let coreTitle = "Core SW5e";
+
+    if (SystemManager.isModuleActive("midi-qol"))
+      coreTitle += ` [supports ${SystemManager.getModuleTitle("midi-qol")}]`;
+
+    let choices = { core: coreTitle };
+    SystemManager.addHandler(choices, "midi-qol");
+    SystemManager.addHandler(choices, "obsidian");
 
     return choices;
   }
 
   /** @override */
   doGetRollHandler(handlerId) {
-    return new Core();
+    let rollHandler;
+    switch (handlerId) {
+      case "midi-qol":
+        rollHandler = new MidiQolSW5e();
+        break;
+      case "obsidian":
+        rollHandler = new ObsidianSW5e();
+        break;
+      case "core":
+      default:
+        rollHandler = new Core();
+        break;
+    }
+
+    if (SystemManager.isModuleActive("magicitems"))
+      rollHandler.addPreRollHandler(new MagicItemsPreRollHandler());
+
+    return rollHandler;
   }
 
   /** @override */
   doRegisterSettings(appName, updateFunc) {
-    settings.register(appName, updateFunc);
+    systemSettings.register(appName, updateFunc);
   }
 }
