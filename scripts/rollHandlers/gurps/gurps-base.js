e2d3680fa17d03afcc6fcfd7450e29b8dbeb2cd7
crnormand <crnormand@bellsouth.net>
2022-03-02 23:07:37 -0500

    Action handler for attributes, Empty roll handler

--- scripts/rollHandlers/gurps/gurps-base.js
+++ scripts/rollHandlers/gurps/gurps-base.js
@@ -30,63 +30,19 @@ export class RollHandlerBaseGURPS extends RollHandler {
 
     async _handleMacros(event, macroType, tokenId, actionId) {
         switch (macroType) {
-            case 'skill':
-                // fall through
-            case 'talent':
-                this.rollTalentMacro(event, tokenId, actionId);
+            case 'attribute':
+                 this.rollAttributeMacro(event, tokenId, actionId);
                 break;
-            case 'inventory':
-                this.rollInventoryMacro(event, tokenId, actionId);
+            case 'blindroll':
+                this.rollAttributeMacro(event, tokenId, actionId);
                 break;
-            case 'toggle':
-                this.toggleDataProperty(event, tokenId, actionId);
         }
     }
 
-    rollTalentMacro(event, tokenId, actionId) {
-        const actor = super.getActor(tokenId);
-        actor.rollPrep({ talentID: actionId});
-    }
-
-    rollInventoryMacro(event, tokenId, actionId) {
-        const actor = super.getActor(tokenId);
-        const item = actor.items.get(actionId);
-        if (item.type === 'weapon') {
-            actor.rollPrep({weaponID: actionId, rolltype: 'attack'});
-        } else if (item.type === 'equipment') {
-            ui.notifications.info("This is not clickable, sorry")
-        } else {
-            //problem
-            Logger.error(item.type, " is not a valid actionId for rolling an inventory item")
-        }
-    }
-
-    toggleDataProperty(event, tokenId, actionId) {
-        const actor = super.getActor(tokenId);
-        if (actionId.includes("tactics")) {
-            const tactic = actionId.split('.')[1];
-            actor.update({
-                data: {
-                    "tactics": {
-                        [tactic]: !actor.data.data.tactics[tactic]
-                    }
-                }
-            })
-        } else {
-            const currentValue = actor.data.data[actionId];
-            const valueType = typeof currentValue;
-            let newValue = valueType === "string" ? this._toggleBooleanString(currentValue) : !currentValue;
-            actor.update({
-                data: {
-                    [actionId]: newValue
-                }
-            })
-        }
-    }
-
-    _toggleBooleanString(val) {
-        if (val.toLowerCase().includes("true")) return "false";
-        if (val.toLowerCase().includes("false")) return "true";
-        return "false";
+    rollAttributeMacro(event, tokenId, actionId) {
+     
+      console.log(GURPS.objToString(event))
+      console.log(tokenId)
+      console.log(actionId) 
     }
 }
