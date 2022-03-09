41189ca363bdfab3af5bbec8394e422002a0ac79
Chris Normand <crnormand@bellsouth.net>
2022-03-09 13:51:29 -0500

    Allow Action name to contain HTML (#177)
    * Initial copy for GURPS
    
    * Action handler for attributes, Empty roll handler
    
    * Implementing GURPS actions
    
    * Added GURPS system
    
    * Reset to bundled version
    
    * Updated GURPS to support user created OTFs
    
    * Allow raw HTML in action name
    
    * Fixed melee/range headers to include usage
    
    * Update defense labels
    
    * Updated to include Quick Notes
    
    * Added Skill/Spell max list size
    
    * Added Maneuver icons

--- scripts/actions/gurps/gurps-actions.js
+++ scripts/actions/gurps/gurps-actions.js
@@ -45,20 +45,13 @@ export class ActionHandlerGURPS extends ActionHandler {
         );
      
       if (Object.keys(actor.data.data.skills).length > 0)
-        this._combineCategoryWithList(
-          result,
-          this.i18n("tokenactionhud.skills"),
-          this._skillsspells(actor, tokenId, 'skills', 'Sk')
-        );
+        this._skillsspells(result, this.i18n("tokenactionhud.skills"), actor, tokenId, 'skills', 'Sk')
         
       if (Object.keys(actor.data.data.spells).length > 0)
-        this._combineCategoryWithList(
-          result,
-          this.i18n("tokenactionhud.spells"),
-          this._skillsspells(actor, tokenId, 'spells', 'Sp')
-        );
+        this._skillsspells(result, this.i18n("tokenactionhud.spells"), actor, tokenId, 'spells', 'Sp')
       
       this._advantages(result, actor, tokenId)
+      this._addQuickNotes(result, actor, tokenId)
       
       if (settings.get('showManeuvers') && !!game.combats.combats.find(c => (c.isActive && !!c.getCombatantByToken(tokenId))))
         this._combineCategoryWithList(
@@ -72,14 +65,44 @@ export class ActionHandlerGURPS extends ActionHandler {
       return result;
     }
     
+  _skillsspells(mainList, label, actor, tokenId, key, otfprefix) {
+    let limit = settings.get('maxListSize')
+    let cnt = 0
+    let columns = 0
+    let result = this.initializeEmptyCategory(key);
+    GURPS.recurselist(actor.data.data[key], (e, k, d) => {
+      if (e.level > 0) {
+        cnt++
+        let attributeCategory = this.initializeEmptySubcategory();
+        let q = '"'
+        if (e.name.includes(q)) q = "'"
+        attributeCategory.actions.push({
+          name: e.name + ' (' + e.level + ')',
+          encodedValue: ["otf", tokenId, otfprefix + ':' + q + e.name + q].join(this.delimiter)
+        }); 
+        this._addNoteOTFs(attributeCategory, tokenId, e.name + ' ' + e.notes)
+        
+        this._combineSubcategoryWithCategory(result, '', attributeCategory);
+        if (cnt >= limit) {
+          cnt = 0
+          columns++
+          this._combineCategoryWithList(mainList, label + (columns > 1 ? '-' + columns :''), result);  
+          result = this.initializeEmptyCategory(key + columns);
+        }
+      }
+    })
+    if (cnt > 0) {
+      columns++
+      this._combineCategoryWithList(mainList, label + (columns > 1 ? '-' + columns :''), result);  
+    }
+  }
+
   _advantages(result, actor, tokenId) {
     let any = false
     let cat = this.initializeEmptyCategory("ads");
     GURPS.recurselist(actor.data.data.ads, (e, k, d) => {
       let attributeCategory = this.initializeEmptySubcategory();
-      this._addNoteOTFs(attributeCategory, tokenId, e.name + ' ' + e.notes)
-
-      if (attributeCategory.actions.length > 0) {
+      if (this._addNoteOTFs(attributeCategory, tokenId, e.name + ' ' + e.notes)) {
         any = true
         this._combineSubcategoryWithCategory(cat, e.name, attributeCategory);
       }
@@ -92,25 +115,15 @@ export class ActionHandlerGURPS extends ActionHandler {
         );
   }
   
-  _skillsspells(actor, tokenId, key, otfprefix) {
-    let result = this.initializeEmptyCategory(key);
-    GURPS.recurselist(actor.data.data[key], (e, k, d) => {
-      if (e.level > 0) {
-        let attributeCategory = this.initializeEmptySubcategory();
-        let q = '"'
-        if (e.name.includes(q)) q = "'"
-        attributeCategory.actions.push({
-          name: e.name + ' (' + e.level + ')',
-          encodedValue: ["otf", tokenId, otfprefix + ':' + q + e.name + q].join(this.delimiter),
-        }); 
-        this._addNoteOTFs(attributeCategory, tokenId, e.name + ' ' + e.notes)
-        
-        this._combineSubcategoryWithCategory(result, '', attributeCategory);
-      }
-    })
-    return result
-  }
-
+  _addQuickNotes(result, actor, tokenId) {
+    let any = false
+    let cat = this.initializeEmptyCategory("qn");
+    let attributeCategory = this.initializeEmptySubcategory();
+    if (this._addNoteOTFs(attributeCategory, tokenId, actor.data.data.additionalresources.qnotes))
+      this._combineSubcategoryWithCategory(cat, '', attributeCategory);
+      this._combineCategoryWithList(result, this.i18n("GURPS.quicknotes"), cat);
+   }
+ 
   _ranged(actor, tokenId) {
     let result = this.initializeEmptyCategory("ranged");
     GURPS.recurselist(actor.data.data.ranged, (e, k, d) => {
@@ -135,21 +148,27 @@ export class ActionHandlerGURPS extends ActionHandler {
 
       this._addNoteOTFs(attributeCategory, tokenId, e.name + ' ' + e.notes)
 
-      this._combineSubcategoryWithCategory(result, e.name, attributeCategory);
+      let n = e.name
+      if (!!e.mode) n += ' (' + e.mode + ')'
+      this._combineSubcategoryWithCategory(result, n, attributeCategory);
     })
     return result
   }
   
   _addNoteOTFs(attributeCategory, tokenId, notes) {
+    let any = false
+    if (!!notes)
       GURPS.gurpslink(notes, false, true).forEach(a => {
-      attributeCategory.actions.push({
-        name: a.text.match(/<span.*>(.*)<\/span>/)[1],
-        encodedValue: ["otf", tokenId, a.action.orig].join(this.delimiter),
-        cssClass: 'standalonggurpslink'
-      }); 
-    })
+        any = true
+        attributeCategory.actions.push({
+          name: a.text, // a.text.match(/<span.*>(.*)<\/span>/)[1],
+          encodedValue: ["otf", tokenId, a.action.orig].join(this.delimiter),
+          //cssClass: 'standalonggurpslink'
+        }); 
+      })
+    return any
   }
- 
+  
   _melee(actor, tokenId) {
     let result = this.initializeEmptyCategory("melee");
     GURPS.recurselist(actor.data.data.melee, (e, k, d) => {
@@ -176,8 +195,9 @@ export class ActionHandlerGURPS extends ActionHandler {
       }); 
 
       this._addNoteOTFs(attributeCategory, tokenId, e.name + ' ' + e.notes)
-      
-      this._combineSubcategoryWithCategory(result, e.name, attributeCategory);
+      let n = e.name
+      if (!!e.mode) n += ' (' + e.mode + ')'
+      this._combineSubcategoryWithCategory(result, n, attributeCategory);
     })
     return result
   }
@@ -185,10 +205,13 @@ export class ActionHandlerGURPS extends ActionHandler {
   _maneuvers(actor, tokenId) {
     let result = this.initializeEmptyCategory("maneuvers");
     let attributeCategory = this.initializeEmptySubcategory();
-    Object.values(GURPS.Maneuvers.getAll()).map(e => this.i18n(e.data.label)).forEach(e => {
+    Object.values(GURPS.Maneuvers.getAll()).forEach(m => {
+//    Object.values(GURPS.Maneuvers.getAll()).map(e => this.i18n(e.data.label)).forEach(e => {
+      let t = this.i18n(m.data.label)
       attributeCategory.actions.push({
-        name: e,
-        encodedValue: ["otf", tokenId, '/man ' + e].join(this.delimiter),
+        name: t,
+        encodedValue: ["otf", tokenId, '/man ' + t].join(this.delimiter),
+        img: m.icon
       }); 
     })   
     this._combineSubcategoryWithCategory(result, '', attributeCategory);
@@ -199,20 +222,20 @@ export class ActionHandlerGURPS extends ActionHandler {
     let result = this.initializeEmptyCategory("defenses");
 
     let cat = this._addDefense(tokenId, this.i18n('GURPS.dodge') + ' (' + actor.data.data.currentdodge + ')', 'DODGE')
-    this._addDefense(tokenId, this.i18n('tokenactionhud.gurps.retreatdodge', 'DODGE +3 ' + this.i18n('GURPS.modifierDodgeRetreat')), cat)
+    this._addDefense(tokenId, this.i18n('tokenactionhud.gurps.retreatdodge') + ' (' + (actor.data.data.currentdodge + 3) + ')', 'DODGE +3 ' + this.i18n('GURPS.modifierDodgeRetreat'), cat)
     this._combineSubcategoryWithCategory(result, '', cat);
     
     if (!!actor.data.data.equippedparry) {
       cat = this._addDefense(tokenId, this.i18n('GURPS.parry') + ' (' + actor.data.data.equippedparry + ')', 'PARRY')
       if (!!actor.data.data.equippedparryisfencing)
-        this._addDefense(tokenId, this.i18n("tokenactionhud.gurps.retreatparryfence"), 'PARRY +3 fencing retreat', cat)
+        this._addDefense(tokenId, this.i18n("tokenactionhud.gurps.retreatparryfence") + ' (' + (actor.data.data.equippedparry + 3) + ')', 'PARRY +3 fencing retreat', cat)
       else
-        this._addDefense(tokenId, this.i18n("tokenactionhud.gurps.retreatparry"), 'PARRY +1 retreating', cat)
+        this._addDefense(tokenId, this.i18n("tokenactionhud.gurps.retreatparry") + ' (' + (actor.data.data.equippedparry + 1) + ')', 'PARRY +1 retreating', cat)
       this._combineSubcategoryWithCategory(result, '', cat);
     }
     if (!!actor.data.data.equippedblock) {
       cat = this._addDefense(tokenId, this.i18n('GURPS.block') + ' (' + actor.data.data.equippedblock + ')', 'BLOCK')
-      this._addDefense(tokenId, this.i18n("tokenactionhud.gurps.retreatblock"), 'BLOCK +1 retreating', cat)
+      this._addDefense(tokenId, this.i18n("tokenactionhud.gurps.retreatblock") + ' (' + (actor.data.data.equippedblock + 1) + ')', 'BLOCK +1 retreating', cat)
       this._combineSubcategoryWithCategory(result, '', cat);
     }
     return result;
