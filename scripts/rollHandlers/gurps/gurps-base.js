ba32d305ddc8495df146f6572c2586f7c272afda
Chris Normand <crnormand@bellsouth.net>
2022-04-29 13:34:32 -0400

    GURPS V4 Support (Right mouse click on damage) (#200)

--- scripts/rollHandlers/gurps/gurps-base.js
+++ scripts/rollHandlers/gurps/gurps-base.js
@@ -33,12 +33,26 @@ export class RollHandlerBaseGURPS extends RollHandler {
           case 'otf':
               this.executeOTF(event, actor, actionId)
               break
+          case 'dam':
+              this.executeOTF(event, actor, actionId, true)
+              break
       }
   }
 
-  async executeOTF(event, actor, otf) {
-    let saved = GURPS.LastActor
-    GURPS.SetLastActor(actor)
-    GURPS.executeOTF(otf).then(e => GURPS.SetLastActor(saved))
+  async executeOTF(event, actor, otf, rightClickDmg = false) {
+    if (rightClickDmg && this.isRightClick(event)) {
+      let action = GURPS.parselink(otf)
+      action.action.calcOnly = true
+      let formula = await GURPS.performAction(action.action, actor)
+      let ev = {... event}
+      ev.preventDefault = () => {}
+      ev.currentTarget = {... ev.currentTarget}
+      ev.currentTarget.innerText = formula
+      GURPS.resolveDamageRoll(ev, actor, formula, '', game.user.isGM, true)
+    } else {
+      let saved = GURPS.LastActor
+      GURPS.SetLastActor(actor)
+      GURPS.executeOTF(otf).then(e => GURPS.SetLastActor(saved))
+    }
   }
 }
