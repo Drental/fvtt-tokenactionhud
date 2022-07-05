5439bcf0498f9c09bdf0ab465d1f76f7c277a816
khaali-dev <github@jgol.fr>
2022-07-05 18:56:20 +0200

    cle en main system support

---
+++ scripts/managers/cleenmain.js
@@ -0,0 +1,34 @@
+import { SystemManager } from "./manager.js";
+import { ActionHandlerCleenmain as ActionHandler } from "../actions/cleenmain/cleenmain-actions.js";
+import { RollHandlerBaseSymbaroum as Core } from "../rollHandlers/cleenmain/cleenmain-base.js";
+import * as settings from "../settings/cleenmain-settings.js";
+
+export class CleenmainSystemManager extends SystemManager {
+  constructor(appName) {
+    super(appName);
+  }
+
+  /** @override */
+  doGetActionHandler(filterManager, categoryManager) {
+    console.log("startup");
+    let actionHandler = new ActionHandler(filterManager, categoryManager);
+    return actionHandler;
+  }
+
+  /** @override */
+  getAvailableRollHandlers() {
+    let choices = { core: "Core Cle en main" };
+
+    return choices;
+  }
+
+  /** @override */
+  doGetRollHandler(handlerId) {
+    return new Core();
+  }
+
+  /** @override */
+  doRegisterSettings(appName, updateFunc) {
+    settings.register(appName, updateFunc);
+  }
+}
