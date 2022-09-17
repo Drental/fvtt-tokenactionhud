45296105dff2dc3cfa2d684df9838e4140b6cd88
Scepfari <63742921+Scepfari@users.noreply.github.com>
2022-09-17 21:12:00 +0200

    Bugfix: #274
     SPACE 1889: Entry "Active Defense" is missing for creatures #274

--- scripts/actions/space1889/space1889-actions.js
+++ scripts/actions/space1889/space1889-actions.js
@@ -90,8 +90,10 @@ export class ActionHandlerSpace1889 extends ActionHandler
 			this._addEntry(tokenId, this.i18n('SPACE1889.Block') + ' (' + actor.system.block.value + ')', 'block', type, category);
 			this._addEntry(tokenId, this.i18n('SPACE1889.Parry') + ' (' + actor.system.parry.value + ')', 'parry', type, category);
 			this._addEntry(tokenId, this.i18n('SPACE1889.Evasion') + ' (' + actor.system.evasion.value + ')', 'evasion', type, category);
-			this._addEntry(tokenId, this.i18n('SPACE1889.ActiveDefense') + ' (' + actor.system.secondaries.defense.activeTotal + ')', 'activeDefense', type, category);
 		}
+		if (actor.type != 'vehicle')
+			this._addEntry(tokenId, this.i18n('SPACE1889.ActiveDefense') + ' (' + actor.system.secondaries.defense.activeTotal + ')', 'activeDefense', type, category);
+			
 		this._addEntry(tokenId, this.i18n('SPACE1889.PassiveDefense') + ' (' + actor.system.secondaries.defense.passiveTotal + ')', 'passiveDefense', type, category);
 		this._addEntry(tokenId, this.i18n('SPACE1889.TalentVolleAbwehr') + ' (' + (actor.system.secondaries.defense.total+4) + ')', 'totalDefense', type, category);
 
